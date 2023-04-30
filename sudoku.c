95109554de70691de6ab166def37e0287dd585a8
Replit user <>
2023-04-30 21:53:03 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -58,7 +58,7 @@ List* get_adj_nodes(Node* n){
       while(j < 9){
         if(n->sudo[i][j] == 0){
           while(cont < 10){
-            
+            list = (List *) malloc(sizeof(List *));
             pushFront(list, aux);
             cont++;
           }
