2947aee975b32681141ddad3cb68d68a7c9fb2b7
Replit user <>
2023-04-30 21:44:19 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -60,7 +60,7 @@ List* get_adj_nodes(Node* n){
           while(cont < 10){
             aux->sudo[i][j] = cont;
             list = (List *) malloc(sizeof(List *));
-            pushCurrent(list, aux);
+            pushBack(list, aux);
             cont++;
           }
           return list;
