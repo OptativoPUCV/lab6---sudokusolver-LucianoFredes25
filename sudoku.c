92e7ded43e09259e06be5bd35c426923ca9bc7df
Replit user <>
2023-05-04 17:07:13 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -120,7 +120,7 @@ Node* DFS(Node* initial, int* cont){
   cont = 0;
   while(is_empty(snack) != 1)
     {
-      cont++;
+      *cont++;
       nodoAux = copy(first(snack));
       popFront(snack);
       
